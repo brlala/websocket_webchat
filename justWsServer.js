ae78912bdb9006c4ce79908d542e7b157c72f120
liheng <liheng@pand.ai>
2020-07-17 00:03:36 +0800

    added basic websocket server

---
+++ justWsServer.js
@@ -0,0 +1,23 @@
+const http = require('http');
+// 3rd party library https://www.npmjs.com/package/ws
+const websocket = require('ws');
+
+const server = http.createServer((req, res) => {
+  res.end('You are connected');
+});
+
+const wss = new websocket.Server({server})
+
+// check on even upon handshake
+wss.on('headers',(headers, req)=>{
+  console.log(headers)
+})
+
+// if anybody connects to the websicket server, it will send
+wss.on('connection', function connection(ws) {
+  ws.send('welcome to websocket server');
+  ws.on('message', function incoming(data) {
+    console.log(data);
+  });
+});
+server.listen(8000);
