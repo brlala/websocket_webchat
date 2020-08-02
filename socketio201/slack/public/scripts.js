06d108b4ab631edd1de3243109cc213e1f99727f
liheng <liheng@pand.ai>
2020-08-02 17:03:37 +0800

    refactoring

--- socketio201/slack/public/scripts.js
+++ socketio201/slack/public/scripts.js
@@ -3,7 +3,6 @@ const socket = io('http://localhost:9000');
 
 // listen for nsList, which is a list of all the namespaces
 socket.on('nsList', (nsData) => {
-  console.log(nsData);
   const namespacesDiv = document.querySelector('.namespaces');
   namespacesDiv.innerHTML = '';
   nsData.forEach((ns) => {
@@ -17,41 +16,5 @@ socket.on('nsList', (nsData) => {
       console.log(`${nsEndpoint}  I should go to now`);
     });
   });
-  const nsSocket = io('http://localhost:9000/wiki');
-  nsSocket.on('nsRoomLoad', (nsRooms) => {
-    // console.log(nsRooms);
-    const roomList = document.querySelector('.room-list');
-    roomList.innerHTML = '';
-    nsRooms.forEach((room) => {
-      let glyph;
-      if (room.privateRoom) {
-        glyph = 'lock';
-      } else {
-        glyph = 'globe';
-      }
-      roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`;
-    });
-    // add click listener to each room
-    const roomNodes = document.getElementsByClassName('room');
-    Array.from(roomNodes).forEach((elem) => {
-      elem.addEventListener('click', (e) => {
-        console.log('Someone clicked on', e.target.innerText);
-      });
-    });
-  });
-});
-socket.on('messageFromServer', (dataFromServer) => {
-  console.log(dataFromServer);
-  socket.emit('messageToServer', {
-    data: 'Data from Client!',
-  });
-});
-
-socket.on('welcome', (dataFromServer) => {
-  console.log(dataFromServer);
-});
-document.querySelector('#message-form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const newMessage = document.querySelector('#user-message').value;
-  socket.emit('newMessageToServer', { text: newMessage });
+  joinNs('/wiki');
 });
