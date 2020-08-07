9fc0f9bf7ffc8119b3549c1cc14c2d4496c14686
liheng <liheng@pand.ai>
2020-08-07 15:54:36 +0800

    added publish and exchange

--- slack v2(redis+rabbitmq)/rabbitmq/3_publish_subscribe/emit_log.js
+++ slack v2(redis+rabbitmq)/rabbitmq/3_publish_subscribe/emit_log.js
@@ -8,17 +8,16 @@ amqp.connect('amqp://localhost', (error0, connection) => {
     if (error1) {
       throw error1;
     }
-    let queue = 'task_queue';
-    let msg = process.argv.slice(2).join(' ') || 'Hello World!';
+    const exchange = 'logs';
+    const msg = process.argv.slice(2).join(' ') || 'Hello World!';
 
-    channel.assertQueue(queue, {
-      durable: true,
+    channel.assertExchange(exchange, 'fanout', {
+      durable: false,
     });
-    channel.sendToQueue(queue, Buffer.from(msg), {
-      persistent: true,
-    });
+    channel.publish(exchange, '', Buffer.from(msg));
     console.log(`[x] Sent ${msg}`);
   });
+
   setTimeout(() => {
     connection.close();
     process.exit(0);
