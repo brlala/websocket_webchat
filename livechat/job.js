e57b235e116bba0c8d8b0ab87cf022f5ffedfa66
liheng <liheng@pand.ai>
2020-08-18 12:59:30 +0800

    emit message to new room

--- livechat/job.js
+++ livechat/job.js
@@ -9,9 +9,9 @@ module.exports = function (msg) {
   const fullMsg = {
     text: msg.data.text,
     time: Date.now(),
-    username: msg.sender_platform_id,
+    username: msg.session_id,
     avatar: 'https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/user.png',
   };
-  namespaces[0].addRoom(new Room(uuidv4(), msg.sender_platform_id, 'Wiki'));
-  io.of('/wiki').to('New Articles').emit('messageToClients', fullMsg);
+  namespaces[0].addRoom(new Room(uuidv4(), msg.session_id, 'Wiki'));
+  io.of('/wiki').to(msg.session_id).emit('messageToClients', fullMsg);
 };
