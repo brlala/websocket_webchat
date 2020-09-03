7eca6cc1ff0f7cb26b9f50df30e2731a71152385
liheng <liheng@pand.ai>
2020-09-03 20:25:55 +0800

    removing console log

--- livechat/controllers/testController.js
+++ livechat/controllers/testController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
 exports.test = async (req, res) => {
-  const { email } = req.body;
+  const { email } = req.payload;
   res.json({
     message: 'authentication succeed',
     email,
