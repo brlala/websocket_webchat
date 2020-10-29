8935142111260570f6fe82c96eae29e954559057
liheng <liheng@pand.ai>
2020-10-29 17:03:31 +0800

    completed password.test.js

--- livechat/test/password.test.js
+++ livechat/test/password.test.js
@@ -1,4 +1,22 @@
 const chai = require('chai');
 const { hashPassword, verifyPassword } = require('../password');
 
-const { expect } = chai;
+const { expect, assert } = chai;
+
+const config = {
+  hashBytes: 32,
+  saltBytes: 16,
+  digest: 'sha512',
+  iterations: 150000,
+};
+describe('passwordManager', () => {
+  it('password encryption and decryption flow', (done) => {
+    const password = 'pѬѬasѪ"§§)("!编/)$=?!°&%)?§"$(§sw汉字编码§"$(§sw汉字方法orФdpѬѬasѪ"§§)("!/)$=?!°&%)?编码方法orФd';
+    hashPassword(password, (err, hash) => {
+      verifyPassword(password, hash, (err1, correct) => {
+        expect(correct).to.equal(true);
+        done();
+      });
+    });
+  });
+});
