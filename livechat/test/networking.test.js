04c32f841ff0843dd05dabe21b20cdba25537469
liheng <liheng@pand.ai>
2020-10-29 17:09:25 +0800

    adding networking test

--- livechat/test/networking.test.js
+++ livechat/test/networking.test.js
@@ -2,3 +2,9 @@ const chai = require('chai');
 const { formatIpAddress } = require('../networking');
 
 const { expect } = chai;
+
+describe('formatIpAddress()', () => {
+  it('172.2.0.0 -> 172-2-0-0', () => {
+    expect(formatIpAddress('172.2.0.0')).to.equal('172-2-0-0');
+  });
+});
