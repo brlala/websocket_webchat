3fba60ded46911c95367f34bfbb1658c78ee164d
liheng <liheng@pand.ai>
2020-10-29 17:09:07 +0800

    added more test to password manager

--- livechat/test/password.test.js
+++ livechat/test/password.test.js
@@ -3,18 +3,23 @@ const { hashPassword, verifyPassword } = require('../password');
 
 const { expect, assert } = chai;
 
-const config = {
-  hashBytes: 32,
-  saltBytes: 16,
-  digest: 'sha512',
-  iterations: 150000,
-};
-describe('passwordManager', () => {
-  it('password encryption and decryption flow', (done) => {
+describe('validator hashPassword() and verifyPassword()', () => {
+  it('Correct password = True', (done) => {
     const password = 'pѬѬasѪ"§§)("!编/)$=?!°&%)?§"$(§sw汉字编码§"$(§sw汉字方法orФdpѬѬasѪ"§§)("!/)$=?!°&%)?编码方法orФd';
     hashPassword(password, (err, hash) => {
       verifyPassword(password, hash, (err1, correct) => {
         expect(correct).to.equal(true);
+        done();
+      });
+    });
+  });
+
+  it('Incorrect password = False', (done) => {
+    let password = 'pѬѬasѪ"§§)("!编/)$=?!°&%)?§"$(§sw汉字编码§"$(§sw汉字方法orФdpѬѬasѪ"§§)("!/)$=?!°&%)?编码方法orФd';
+    hashPassword(password, (err, hash) => {
+      password = 'pѬѬasѪ"§§)("!编/)$=?!°&%)?§"$(§sw汉字编码§"$(§sw汉字方法orФdpѬѬasѪ"§§)("!/)$=?!°&%)?编码方法orФe';
+      verifyPassword(password, hash, (err1, correct) => {
+        expect(correct).to.equal(false);
         done();
       });
     });
